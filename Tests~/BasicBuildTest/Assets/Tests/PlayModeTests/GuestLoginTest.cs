b6079b920fc6d6ba59d81bebf5d4e3ac3da67116
Erik Bylund <erik@lootlocker.io>
2022-09-15 14:59:07 +0200

    Make api key and domain key for the test game supplied by github secret

--- Tests~/BasicBuildTest/Assets/Tests/PlayModeTests/GuestLoginTest.cs
+++ Tests~/BasicBuildTest/Assets/Tests/PlayModeTests/GuestLoginTest.cs
@@ -20,9 +20,6 @@ namespace Tests
             var guestLogin = guestLoginGO.AddComponent<GuestLogin>();
             guestLogin.loginInformationText = loginInformationText;
             guestLogin.playerIdText = playerIdText;
-            guestLogin.apiKey="964f802f3d6adf7b496fac34c592c69243641c4e";
-            guestLogin.domainKey="qes7624r";
-            guestLogin.login("Test");
             yield return new WaitUntil(() => guestLogin.isDone());
             Assert.IsTrue(loginInformationText.text.Contains("Guest session started"));
             Assert.IsTrue(playerIdText.text.Contains("Player ID: "));
