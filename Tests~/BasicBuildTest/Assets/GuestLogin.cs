d615ab660df5716e42cae9e9f1e67334f8236a41
Erik Bylund <erik@lootlocker.io>
2022-09-15 14:59:07 +0200

    Make api key and domain key for the test game supplied by github secret

--- Tests~/BasicBuildTest/Assets/GuestLogin.cs
+++ Tests~/BasicBuildTest/Assets/GuestLogin.cs
@@ -10,52 +10,67 @@ public class GuestLogin : MonoBehaviour
     public Text playerIdText;
     public string apiKey;
     public string domainKey;
-    private bool isRequestDone;
-    private bool isLoggedIn;
+    private bool isRequestDone = false;
+    private bool isRequestInProgress = false;
+    private bool isLoggedIn = false;
     
     void Start()
     {
-        //login("Start");
+        login("Start");
     }
 
     void FixedUpdate() {
-        login("FixedUpdate");
+        //login("FixedUpdate");
     }
 
     public void login(string source) {
-        if(isLoggedIn) {
+        if(isLoggedIn || isRequestInProgress) {
+            Debug.Log("Not in a state to log in, skipping login request from " + source);
             return;
         }
+        Debug.Log("Starting log in request from " + source);
+        
         isRequestDone = false;
+        isRequestInProgress = true;
         initSDK();
 
-        //Debug.LogError("From " + source);
         LootLockerSDKManager.StartGuestSession((response) =>
         {
             if(response.success)
             {
                 loginInformationText.text = "Guest session started";
+                Debug.Log(loginInformationText.text);
                 playerIdText.text = "Player ID: "+response.player_id.ToString();
+                Debug.Log(playerIdText.text);
                 isRequestDone = true;
                 isLoggedIn = true;
+                isRequestInProgress = false;
             }
             else
             {
                 loginInformationText.text = "Error: " + response.Error;
+                Debug.Log(loginInformationText.text);
                 isRequestDone = true;
+                isRequestInProgress = false;
             }
         });
     }
 
     private void initSDK() {
         string[] args = System.Environment.GetCommandLineArgs ();
-        if(args.Length != 2 && (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(domainKey))) {
+        for(int i = 0; i < args.Length; i++) {
+            if(args[i] == "-apikey") {
+                apiKey = args[i+1];
+            } else if (args[i] == "-domainkey") {
+                domainKey = args[i+1];
+            }
+        }
+        if((string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(domainKey))) {
             Debug.LogError("Can't run because no api key or domain key supplied");
             return;
         }
-        var localApiKey = args.Length != 2 ? apiKey : args[0];
-        var localDomainKey = args.Length != 2 ? domainKey : args[1];
-        LootLockerSDKManager.Init(localApiKey, "0.0.0.1", LootLocker.LootLockerConfig.platformType.Android, true, localDomainKey);
+        Debug.Log("Making log in request with apiKey: " + apiKey + " and domainKey: " + domainKey);
+        LootLockerSDKManager.Init(apiKey, "0.0.0.1", LootLocker.LootLockerConfig.platformType.Android, true, domainKey);
         LootLocker.LootLockerConfig.current.currentDebugLevel = LootLocker.LootLockerConfig.DebugLevel.All;
     }
 
