a8a447ac5a2a74d75f15360fc02b2525ae56593c
Erik Bylund <erik@lootlocker.io>
2022-09-15 11:07:06 +0200

    Add Unity Project with basic smoke test: BasicBuildTest

---
+++ Tests~/BasicBuildTest/Assets/GuestLogin.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LootLocker.Requests;
+using UnityEngine.UI;
+
+public class GuestLogin : MonoBehaviour
+{
+    public Text loginInformationText;
+    public Text playerIdText;
+    public string apiKey;
+    public string domainKey;
+    private bool isRequestDone;
+    private bool isLoggedIn;
+    
+    void Start()
+    {
+        //login("Start");
+    }
+
+    void FixedUpdate() {
+        login("FixedUpdate");
+    }
+
+    public void login(string source) {
+        if(isLoggedIn) {
+            return;
+        }
+        isRequestDone = false;
+        initSDK();
+
+        //Debug.LogError("From " + source);
+        LootLockerSDKManager.StartGuestSession((response) =>
+        {
+            if(response.success)
+            {
+                loginInformationText.text = "Guest session started";
+                playerIdText.text = "Player ID: "+response.player_id.ToString();
+                isRequestDone = true;
+                isLoggedIn = true;
+            }
+            else
+            {
+                loginInformationText.text = "Error: " + response.Error;
+                isRequestDone = true;
+            }
+        });
+    }
+
+    private void initSDK() {
+        string[] args = System.Environment.GetCommandLineArgs ();
+        if(args.Length != 2 && (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(domainKey))) {
+            Debug.LogError("Can't run because no api key or domain key supplied");
+            return;
+        }
+        var localApiKey = args.Length != 2 ? apiKey : args[0];
+        var localDomainKey = args.Length != 2 ? domainKey : args[1];
+        LootLockerSDKManager.Init(localApiKey, "0.0.0.1", LootLocker.LootLockerConfig.platformType.Android, true, localDomainKey);
+        LootLocker.LootLockerConfig.current.currentDebugLevel = LootLocker.LootLockerConfig.DebugLevel.All;
+    }
+
+    public bool isDone() {
+        return isRequestDone;
+    }
+}
---
+++ Tests~/BasicBuildTest/Assets/Tests/PlayModeTests/GuestLoginTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class GuestLoginTest
+    {
+        [UnityTest]
+        public IEnumerator GuestUserCanLogIn()
+        {
+            var guestLoginGO = new GameObject();
+            var loginInformationGO = new GameObject();
+            var playerIdGO = new GameObject();
+            var loginInformationText = loginInformationGO.AddComponent<Text>();
+            var playerIdText = playerIdGO.AddComponent<Text>();
+            var guestLogin = guestLoginGO.AddComponent<GuestLogin>();
+            guestLogin.loginInformationText = loginInformationText;
+            guestLogin.playerIdText = playerIdText;
+            guestLogin.apiKey="964f802f3d6adf7b496fac34c592c69243641c4e";
+            guestLogin.domainKey="qes7624r";
+            guestLogin.login("Test");
+            yield return new WaitUntil(() => guestLogin.isDone());
+            Assert.IsTrue(loginInformationText.text.Contains("Guest session started"));
+            Assert.IsTrue(playerIdText.text.Contains("Player ID: "));
+        }
+    }
+}
