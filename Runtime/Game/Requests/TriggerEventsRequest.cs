5440dbd41e8dad6e175d36b5f09e6e9ea7434c48
DESKTOP-05DML1U\johan <johannes@lootlocker.io>
2022-09-19 11:19:24 +0200

    Updated triggers response with the correct json structure

--- Runtime/Game/Requests/TriggerEventsRequest.cs
+++ Runtime/Game/Requests/TriggerEventsRequest.cs
@@ -17,9 +17,54 @@ namespace LootLocker.Requests
     {
         public bool check_grant_notifications { get; set; }
         public LootLockerXp xp { get; set; }
-        public LootLockerLevel[] levels { get; set; }
+        public LootLockerLevel [] levels { get; set; }
+
+        public LootLockerGrantedAssets [] granted_assets {get; set;}
     }
 
+    public class LootLockerGrantedAssets
+    {
+        public int id;
+        public string uuid;
+        public string name;
+        public bool active;
+        public bool purchasable;
+        public int price;
+        public string sales_price;
+        public string display_price;
+        public string context;
+        public string context_id;
+        public int[] character_classes;
+        public string unlocks_context;
+        public bool detachable;
+        public string updated;
+        public string marked_new;
+        public int default_variation_id;
+        public string description;
+        public LootLockerDefault_Loadouts default_Loadouts;
+        public string links;
+        public LootLockerStorage[] storage;
+        public string rarity;
+        public string popular;
+        public int popularity_score;
+        public string package_contents;
+        public bool unique_instance;
+        public string external_identifiers;
+        public string rental_options;
+        public LootLockerEnums.AssetFilter[] filters;
+        public LootLockerFile [] files;
+        public string [] data_entities;
+        public LootLockerHeroEquipExceptions hero_equip_exceptions;
+        public string asset_candidate;
+        public int drop_table_max_picks;
+    }
+
+    public class LootLockerHeroEquipExceptions
+    {
+        public bool can_equip;
+        public int hero_id;
+        public string name;
+    }
 
     public class LootLockerListingAllTriggersResponse : LootLockerResponse
     {
