40dff35a4dfa000e3cd1f8d1c54ec0388f3a899a
Erik Bylund <erik@lootlocker.io>
2022-10-20 16:03:43 +0200

    Don't set session header if token is empty

--- Runtime/Client/LootLockerServerRequest.cs
+++ Runtime/Client/LootLockerServerRequest.cs
@@ -164,8 +164,14 @@ namespace LootLocker
             if (useAuthToken)
             {
                 headers = new Dictionary<string, string>();
-                headers.Add(callerRole == LootLocker.LootLockerEnums.LootLockerCallerRole.Admin ? "x-auth-token" : "x-session-token",
-                    callerRole == LootLocker.LootLockerEnums.LootLockerCallerRole.Admin ? LootLockerConfig.current.adminToken : LootLockerConfig.current.token);
+                if (callerRole == LootLocker.LootLockerEnums.LootLockerCallerRole.Admin && !string.IsNullOrEmpty(LootLockerConfig.current.adminToken))
+                {
+                    headers.Add("x-auth-token", LootLockerConfig.current.adminToken);
+                }
+                else if (callerRole != LootLocker.LootLockerEnums.LootLockerCallerRole.Admin && !string.IsNullOrEmpty(LootLockerConfig.current.token))
+                {
+                    headers.Add("x-session-token", LootLockerConfig.current.token);
+                }
             }
 
             if (LootLockerConfig.current != null)
