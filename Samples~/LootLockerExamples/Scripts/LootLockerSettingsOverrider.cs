7bf7efd0fe6bcada8bb138248736bf874a3e539b
Erik Bylund <erik@lootlocker.io>
2023-02-16 11:33:16 +0100

    Fix samples

--- Samples~/LootLockerExamples/Scripts/LootLockerSettingsOverrider.cs
+++ Samples~/LootLockerExamples/Scripts/LootLockerSettingsOverrider.cs
@@ -5,7 +5,7 @@ public static class LootLockerSettingsOverrider
 {
     public static void OverrideSettings()
     {
-        LootLockerSDKManager.Init("9e4bda3a853f1eb6342f1dfd289c1c9531c21998", "0.0.0.1", true, "2112hpxu");
+        LootLockerSDKManager.Init("dev_9e4bda3a853f1eb6342f1dfd289c1c9531c21998", "0.0.0.1", "2112hpxu");
         LootLocker.LootLockerConfig.current.currentDebugLevel = LootLocker.LootLockerConfig.DebugLevel.All;
     }
 }
