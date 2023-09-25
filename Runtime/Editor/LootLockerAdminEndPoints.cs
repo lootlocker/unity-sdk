5798a6e0c9ef8f4e9b520c44fba999d8e9a461e5
Mikkel Sørensen <kaktusdrengen@outlook.dk>
2023-09-25 10:51:51 +0200

    Add admin endpoints and small fixes.

---
+++ Runtime/Editor/LootLockerAdminEndPoints.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace LootLocker
+{
+    public class LootLockerAdminEndPoints
+    {
+        [Header("API Keys")]
+        public static EndPointClass adminExtensionGetAllKeys = new EndPointClass("game/{0}/api_keys", LootLockerHTTPMethod.GET);
+        public static EndPointClass adminExtensionCreateKey = new EndPointClass("game/{0}/api_keys", LootLockerHTTPMethod.POST);
+
+        [Header("Admin Authentication")]
+        public static EndPointClass adminExtensionLogin = new EndPointClass("v1/session", LootLockerHTTPMethod.POST);
+        public static EndPointClass adminExtensionMFA = new EndPointClass("v1/2fa", LootLockerHTTPMethod.POST);
+
+        [Header("User Roles")]
+        public static EndPointClass adminExtensionGetUserRole = new EndPointClass("roles/{0}", LootLockerHTTPMethod.GET);
+    }
+}
+#endif
