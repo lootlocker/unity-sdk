290851ac6416121236065ec63254ce4b91301a25
Mikkel Sørensen <kaktusdrengen@outlook.dk>
2024-01-23 10:44:11 +0100

    Add uuid field to CommonAsset class

--- Runtime/Game/Requests/AssetRequest.cs
+++ Runtime/Game/Requests/AssetRequest.cs
@@ -112,6 +112,7 @@ namespace LootLocker.Requests
     public class LootLockerCommonAsset : LootLockerResponse
     {
         public int id { get; set; }
+        public string uuid { get; set; }
         public string name { get; set; }
         public bool active { get; set; }
         public bool purchasable { get; set; }
