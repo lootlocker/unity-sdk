b18594898caf206bd1d645d7651c8180490fcb00
Mikkel Sørensen <kaktusdrengen@outlook.dk>
2024-01-25 09:25:42 +0100

    Add uuid to Context response

--- Runtime/Game/Requests/AssetRequest.cs
+++ Runtime/Game/Requests/AssetRequest.cs
@@ -179,6 +179,7 @@ namespace LootLocker.Requests
     public class LootLockerContext
     {
         public int id { get; set; }
+        public string uuid { get; set; }
         public string name { get; set; }
         public string friendly_name { get; set; }
         public bool detachable { get; set; }
