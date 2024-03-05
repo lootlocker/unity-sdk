09c52c610d79243bf3a82f384c122637ac09bdbc
Mikkel Sørensen <kaktusdrengen@outlook.dk>
2024-03-05 20:23:52 +0100

    Fixed lost references

--- Runtime/Editor/LootLockerAdminEndPoints.cs
+++ Runtime/Editor/LootLockerAdminEndPoints.cs
@@ -13,8 +13,10 @@ namespace LootLocker
         public static EndPointClass adminExtensionLogin = new EndPointClass("v1/session", LootLockerHTTPMethod.POST);
         public static EndPointClass adminExtensionMFA = new EndPointClass("v1/2fa", LootLockerHTTPMethod.POST);
 
-        [Header("User Roles")]
+        [Header("User Information")]
+        public static EndPointClass adminExtensionUserInformation = new EndPointClass("v1/user/all", LootLockerHTTPMethod.GET);
         public static EndPointClass adminExtensionGetUserRole = new EndPointClass("roles/{0}", LootLockerHTTPMethod.GET);
+        public static EndPointClass adminExtensionGetGameInformation = new EndPointClass("/game/{0}", LootLockerHTTPMethod.GET);
     }
 }
 #endif
