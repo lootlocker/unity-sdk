073da7906d4c468d804618647433b37f977b65ea
Mikkel Sørensen <kaktusdrengen@outlook.dk>
2024-03-07 15:47:08 +0100

    Final draft hopefully??

--- Runtime/Editor/LootLockerEditorData.cs
+++ Runtime/Editor/LootLockerEditorData.cs
@@ -1,76 +1,87 @@
 
 #if UNITY_EDITOR && UNITY_2021_3_OR_NEWER
 using UnityEditor;
-namespace LootLocker.Extension {
-
-
-public class LootLockerEditorData
+namespace LootLocker.Extension
 {
-    private static string prefix = PlayerSettings.productGUID.ToString() + ".LootLocker.";
 
-    private static string adminToken = "AdminToken";
-    private static string selectedGameID = "SelectedGameID";
-    private static string environment = "Environment";
-    private static string firstTimeWelcome = "FirstTimeWelcome";
-    private static string newSession = "NewSession";
-
-    public static void ClearLootLockerPrefs()
+    public class LootLockerEditorData
     {
-        EditorPrefs.DeleteKey(prefix + adminToken);
-        EditorPrefs.DeleteKey(prefix + selectedGameID);
-        EditorPrefs.DeleteKey(prefix + environment);
-        EditorPrefs.DeleteKey(prefix + firstTimeWelcome);
-        EditorPrefs.DeleteKey(prefix + newSession);
-    }
+        private static string prefix = PlayerSettings.productGUID.ToString() + ".LootLocker.";
 
-    public static void SetAdminToken(string _adminToken)
-    {
-        EditorPrefs.SetString(prefix + adminToken, _adminToken);
-        EditorPrefs.SetBool(prefix + firstTimeWelcome, false);
-        EditorPrefs.SetBool(prefix + newSession, true);
-    }
+        private static string adminToken = "AdminToken";
+        private static string selectedGameID = "SelectedGameID";
+        private static string selectedGameName = "SelectedGameName";
+        private static string environment = "Environment";
+        private static string firstTimeWelcome = "FirstTimeWelcome";
+        private static string newSession = "NewSession";
 
-    public static string GetAdminToken()
-    {
-        return EditorPrefs.GetString(prefix + adminToken);
-    }
+        public static void ClearLootLockerPrefs()
+        {
+            EditorPrefs.DeleteKey(prefix + adminToken);
+            EditorPrefs.DeleteKey(prefix + selectedGameID);
+            EditorPrefs.DeleteKey(prefix + selectedGameName);
+            EditorPrefs.DeleteKey(prefix + environment);
+            EditorPrefs.DeleteKey(prefix + firstTimeWelcome);
+            EditorPrefs.DeleteKey(prefix + newSession);
+        }
 
-    public static void SetSelectedGame(string _selectedGame)
-    {
-        EditorPrefs.SetInt(prefix + selectedGameID, int.Parse(_selectedGame));
-    }
+        public static void SetAdminToken(string _adminToken)
+        {
+            EditorPrefs.SetString(prefix + adminToken, _adminToken);
+            EditorPrefs.SetBool(prefix + firstTimeWelcome, false);
+            EditorPrefs.SetBool(prefix + newSession, true);
+        }
 
-    public static int GetSelectedGame()
-    {
-       return EditorPrefs.GetInt(prefix + selectedGameID);
-    }
+        public static string GetAdminToken()
+        {
+            return EditorPrefs.GetString(prefix + adminToken);
+        }
 
-    public static void SetEnvironmentStage()
-    {
-        EditorPrefs.SetString(prefix + environment, "Stage");
-    }
+        public static void SetSelectedGame(string _selectedGame)
+        {
+            EditorPrefs.SetInt(prefix + selectedGameID, int.Parse(_selectedGame));
+        }
+        public static void SetSelectedGameName(string _selectedGameName)
+        {
+            EditorPrefs.SetString(prefix + selectedGameName, _selectedGameName);
+        }
 
-    public static void SetEnvironmentLive()
-    {
-        EditorPrefs.SetString(prefix + environment, "Live");
-    }
+        public static int GetSelectedGame()
+        {
+            return EditorPrefs.GetInt(prefix + selectedGameID);
+        }
 
-    public static bool IsEnvironmentStage()
-    {
-        return EditorPrefs.GetString(prefix + environment).Equals("Stage");
-    }
+        public static string GetSelectedGameName()
+        {
+            return EditorPrefs.GetString(prefix + selectedGameName);
+        }
 
-    public static bool ShouldAutoShowWindow()
-    {
-        return EditorPrefs.GetBool(prefix + firstTimeWelcome);
-    }
+        public static void SetEnvironmentStage()
+        {
+            EditorPrefs.SetString(prefix + environment, "Stage");
+        }
 
-    public static bool IsNewSession()
-    {
-        bool result = EditorPrefs.GetBool(prefix + newSession, false);
-        EditorPrefs.SetBool(prefix + newSession, false);
-        return result;
+        public static void SetEnvironmentLive()
+        {
+            EditorPrefs.SetString(prefix + environment, "Live");
+        }
+
+        public static bool IsEnvironmentStage()
+        {
+            return EditorPrefs.GetString(prefix + environment).Equals("Stage");
+        }
+
+        public static bool ShouldAutoShowWindow()
+        {
+            return EditorPrefs.GetBool(prefix + firstTimeWelcome);
+        }
+
+        public static bool IsNewSession()
+        {
+            bool result = EditorPrefs.GetBool(prefix + newSession, false);
+            EditorPrefs.SetBool(prefix + newSession, false);
+            return result;
+        }
     }
-}
 }
 #endif
