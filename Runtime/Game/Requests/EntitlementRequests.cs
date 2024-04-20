1ac313b8e6b4e639d20339167bc2177dba327743
Erik Bylund <erik@lootlocker.com>
2024-04-20 10:15:11 +0700

    Document fields for entitlement history

--- Runtime/Game/Requests/EntitlementRequests.cs
+++ Runtime/Game/Requests/EntitlementRequests.cs
@@ -1,4 +1,47 @@
-﻿namespace LootLocker.Requests
+﻿using LootLocker.LootLockerEnums;
+
+namespace LootLocker.LootLockerEnums
+{
+    /// <summary>
+    /// An enum with the supported stores that can generate entitlements
+    /// </summary>
+    public enum LootLockerEntitlementHistoryListingStore
+    {
+        None = 0,
+        Apple_app_store = 1,
+        Google_play_store = 2,
+        Steam_store = 3,
+        Playstation_network = 4,
+        Nintendo_eshop = 5,
+        Lootlocker = 6
+    };
+
+    /// <summary>
+    /// Status of the entitlement
+    /// </summary>
+    public enum LootLockerEntitlementHistoryListingStatus
+    {
+        None = 0,
+        Active = 1,
+        Pending = 2,
+        Expired = 3,
+        Canceled = 4,
+        Refunded = 5
+    };
+
+    /// <summary>
+    /// Status of the entitlement
+    /// </summary>
+    public enum LootLockerEntitlementHistoryListingType
+    {
+        Undefined = 0,
+        One_time_purchase = 1,
+        Leaderboard_reward = 2,
+        Subscription = 3
+    };
+}
+
+namespace LootLocker.Requests
 {
     //==================================================
     // Data Definitions
@@ -10,34 +53,34 @@
     {
 
         /// <summary>
-        /// TODO: Document
+        /// When this item was created
         /// </summary>
-        public string created_at { get; set; }
+        public string Created_at { get; set; }
 
         /// <summary>
-        /// TODO: Document
+        /// What kind of reward this item is
         /// </summary>
-        public string reward_kind { get; set; }
+        public string Reward_kind { get; set; }
 
         /// <summary>
-        /// TODO: Document
+        /// The unique identifier of this specific item
         /// </summary>
-        public string id { get; set; }
+        public string Id { get; set; }
 
         /// <summary>
-        /// TODO: Document
+        /// The id of the reward this item is in
         /// </summary>
-        public string reward_id { get; set; }
+        public string Reward_id { get; set; }
 
         /// <summary>
-        /// TODO: Document
+        /// The id of the catalog item that this item is in
         /// </summary>
-        public string catalog_id { get; set; }
+        public string Catalog_id { get; set; }
 
         /// <summary>
-        /// TODO: Document
+        /// Whether this item is purchasable
         /// </summary>
-        public bool purchasable { get; set; }
+        public bool Purchasable { get; set; }
     }
 
     /// <summary>
@@ -46,14 +89,14 @@
     {
 
         /// <summary>
-        /// TODO: Document
+        /// The key of this pair, describes what the value is
         /// </summary>
-        public string key { get; set; }
+        public string Key { get; set; }
 
         /// <summary>
-        /// TODO: Document
+        /// The value of this pair, contains the information of the metadata
         /// </summary>
-        public string value { get; set; }
+        public string Value { get; set; }
     }
 
     /// <summary>
@@ -62,24 +105,19 @@
     {
 
         /// <summary>
-        /// TODO: Document
+        /// When this reward was created
         /// </summary>
-        public string created_at { get; set; }
+        public string Created_at { get; set; }
 
         /// <summary>
-        /// TODO: Document
+        /// The id of this entitlement
         /// </summary>
-        public string entitlement_id { get; set; }
+        public string Entitlement_id { get; set; }
 
         /// <summary>
-        /// TODO: Document
+        /// The unique identifier of this reward
         /// </summary>
-        public string id { get; set; }
-
-        /// <summary>
-        /// TODO: Document
-        /// </summary>
-        public string reward_id { get; set; }
+        public string Id { get; set; }
     }
 
     //==================================================
@@ -92,14 +130,14 @@
     {
 
         /// <summary>
-        /// TODO: Document
+        /// List of entitlement history entries
         /// </summary>
-        public LootLockerEntitlementListing[] listings { get; set; }
-        
+        public LootLockerEntitlementListing[] Listings { get; set; }
+
         /// <summary>
-        /// TODO: Document
+        /// Pagination data to use for subsequent requests
         /// </summary>
-        public LootLockerPaginationResponse<string> pagination { get; set; }
+        public LootLockerPaginationResponse<string> Pagination { get; set; }
     }
     
     /// <summary>
@@ -108,48 +146,44 @@
     {
         
         /// <summary>
-        /// TODO: Document
+        /// When this entitlement listing was created
         /// </summary>
-        public string created_at { get; set; }
+        public string Created_at { get; set; }
         
         /// <summary>
-        /// TODO: Document
+        /// The unique identifier of this entitlement listing
         /// </summary>
-        public string id { get; set; }
-        
+        public string Id { get; set; }
+
         /// <summary>
-        /// TODO: Document
+        /// List of items in this entitlement (items are related to the catalog) 
         /// </summary>
-        public LootLockerEntitlementHistoryItem[] items { get; set; }
-        
+        public LootLockerEntitlementHistoryItem[] Items { get; set; }
+
         /// <summary>
-        /// TODO: Document
+        /// List of rewards in this entitlement (these are rewards from systems such as leaderboards, progressions, etc.) 
         /// </summary>
-        public LootLockerEntitlementHistoryMetadata[] metadata { get; set; }
-        
+        public LootLockerEntitlementHistoryReward[] Rewards { get; set; }
+
         /// <summary>
-        /// TODO: Document
+        /// Metadata related to this entitlement listing
+        /// This array consists of key value pairs and contains various pieces of information about the entitlement, such as information from third party stores etc.
         /// </summary>
-        public string player_id { get; set; }
-        
+        public LootLockerEntitlementHistoryMetadata[] Metadata { get; set; }
+
         /// <summary>
-        /// TODO: Document
+        /// The status of this entitlement listing
         /// </summary>
-        public LootLockerEntitlementHistoryReward[] rewards { get; set; }
-        
+        public LootLockerEntitlementHistoryListingStatus Status { get; set; } = LootLockerEntitlementHistoryListingStatus.None;
+
         /// <summary>
-        /// TODO: Document
+        /// Which store (if any) that this entitlement listing relates to
         /// </summary>
-        public string status { get; set; }
-        
+        public LootLockerEntitlementHistoryListingStore Store { get; set; } = LootLockerEntitlementHistoryListingStore.None;
+
         /// <summary>
-        /// TODO: Document
+        /// Which type this entitlement listing is
         /// </summary>
-        public string store { get; set; }
-        
-        /// <summary>
-        /// TODO: Document
-        /// </summary>
-        public string type { get; set; }
+        public LootLockerEntitlementHistoryListingType Type { get; set; } = LootLockerEntitlementHistoryListingType.Undefined;
     }
 }
