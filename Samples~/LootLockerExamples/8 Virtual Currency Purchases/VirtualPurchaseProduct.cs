0f641cd4c982d49106cc3ce0c7bf19bda669b5aa
Mikkel Sørensen <mikkeljonassoerensen@gmail.com>
2024-08-12 13:51:21 +0200

    Added virtual currency purchasing to Samples

---
+++ Samples~/LootLockerExamples/8 Virtual Currency Purchases/VirtualPurchaseProduct.cs
@@ -0,0 +1,36 @@
+using LootLocker;
+using LootLocker.Requests;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualPurchaseProduct : MonoBehaviour
+{
+
+    public Text productName;
+    public Text price;
+
+    private string productID;
+
+    public void CreateProduct(string _productName, int _price, string _productID)
+    {
+        productName.text = "Buy " + _productName;
+        price.text = "Price: " + _price.ToString();
+
+        productID = _productID;
+    }
+
+    public void Purchase()
+    {
+        LootLockerSDKManager.LootLockerPurchaseSingleCatalogItem(LootLockerVirtualStore.Instance.walletID, productID, 1, (response) =>
+        {
+            if (!response.success)
+            {
+                Debug.Log("Could not buy catalog item");
+                return;
+            }
+
+            LootLockerVirtualStore.Instance.GetPlayerGold();
+        });
+    }
+
+}
