00899b19c26310690df329525fb98f2f2b9ea6a4
Erik Bylund <erik@lootlocker.io>
2024-11-12 12:08:54 +0100

    refactor: Move caller role placement into the endpoint definitions

--- Runtime/Editor/LootLockerAdminEndPoints.cs
+++ Runtime/Editor/LootLockerAdminEndPoints.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using LootLocker.LootLockerEnums;
 
 #if UNITY_EDITOR && UNITY_2021_3_OR_NEWER
 namespace LootLocker
@@ -6,17 +7,17 @@ namespace LootLocker
     public class LootLockerAdminEndPoints
     {
         [Header("API Keys")]
-        public static EndPointClass adminExtensionGetAllKeys = new EndPointClass("game/{0}/api_keys", LootLockerHTTPMethod.GET);
-        public static EndPointClass adminExtensionCreateKey = new EndPointClass("game/{0}/api_keys", LootLockerHTTPMethod.POST);
+        public static EndPointClass adminExtensionGetAllKeys = new EndPointClass("game/{0}/api_keys", LootLockerHTTPMethod.GET, LootLockerCallerRole.Admin);
+        public static EndPointClass adminExtensionCreateKey = new EndPointClass("game/{0}/api_keys", LootLockerHTTPMethod.POST, LootLockerCallerRole.Admin);
 
         [Header("Admin Authentication")]
-        public static EndPointClass adminExtensionLogin = new EndPointClass("v1/session", LootLockerHTTPMethod.POST);
-        public static EndPointClass adminExtensionMFA = new EndPointClass("v1/2fa", LootLockerHTTPMethod.POST);
+        public static EndPointClass adminExtensionLogin = new EndPointClass("v1/session", LootLockerHTTPMethod.POST, LootLockerCallerRole.Admin);
+        public static EndPointClass adminExtensionMFA = new EndPointClass("v1/2fa", LootLockerHTTPMethod.POST, LootLockerCallerRole.Admin);
 
         [Header("User Information")]
-        public static EndPointClass adminExtensionUserInformation = new EndPointClass("v1/user/all", LootLockerHTTPMethod.GET);
-        public static EndPointClass adminExtensionGetUserRole = new EndPointClass("roles/{0}", LootLockerHTTPMethod.GET);
-        public static EndPointClass adminExtensionGetGameInformation = new EndPointClass("v1/game/{0}", LootLockerHTTPMethod.GET);
+        public static EndPointClass adminExtensionUserInformation = new EndPointClass("v1/user/all", LootLockerHTTPMethod.GET, LootLockerCallerRole.Admin);
+        public static EndPointClass adminExtensionGetUserRole = new EndPointClass("roles/{0}", LootLockerHTTPMethod.GET, LootLockerCallerRole.Admin);
+        public static EndPointClass adminExtensionGetGameInformation = new EndPointClass("v1/game/{0}", LootLockerHTTPMethod.GET, LootLockerCallerRole.Admin);
     }
 }
 #endif
