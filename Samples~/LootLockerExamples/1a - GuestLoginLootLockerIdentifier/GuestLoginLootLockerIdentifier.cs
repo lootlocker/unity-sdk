85750e35fd84fc8d973cdb2d21be4548c3ff8299
Erik Bylund <erik@lootlocker.io>
2024-12-03 14:03:07 +0100

    fix: Adress null reference exception UI elements in samples

--- Samples~/LootLockerExamples/1a - GuestLoginLootLockerIdentifier/GuestLoginLootLockerIdentifier.cs
+++ Samples~/LootLockerExamples/1a - GuestLoginLootLockerIdentifier/GuestLoginLootLockerIdentifier.cs
@@ -1,26 +1,33 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using LootLocker.Requests;
 using UnityEngine.UI;
 
-namespace LootLocker {
-public class GuestLoginLootLockerIdentifier : MonoBehaviour
+namespace LootLocker
+{
+    public class GuestLoginLootLockerIdentifier : MonoBehaviour
 {
     public Text loginInformationText;
     public Text playerIdText;
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(DoGuestLogin());
+    }
+
+    IEnumerator DoGuestLogin()
+    {
+        // Wait until end of frame to ensure that the UI has been loaded
+        yield return new WaitForEndOfFrame();
         /* 
-         * Override settings to use the Example game setup
-         */
+        * Override settings to use the Example game setup
+        */
         LootLockerSettingsOverrider.OverrideSettings();
 
         /* Start guest session without an identifier.
-         * LootLocker will create an identifier for the user and store it in PlayerPrefs.
-         * If you want to create a new player when testing, you can use PlayerPrefs.DeleteKey("LootLockerGuestPlayerID");
-         */
+        * LootLocker will create an identifier for the user and store it in PlayerPrefs.
+        * If you want to create a new player when testing, you can use PlayerPrefs.DeleteKey("LootLockerGuestPlayerID");
+        */
         LootLockerSDKManager.StartGuestSession((response) =>
         {
             if(response.success)
@@ -30,7 +37,7 @@ public class GuestLoginLootLockerIdentifier : MonoBehaviour
             }
             else
             {
-                loginInformationText.text = "Error" + response.errorData.message;
+                loginInformationText.text = "Error" + response.errorData.ToString();
             }
         });
     }
