c1d7c87be4d87210f4064b8758b3b1eb5bf730cb
Erik Bylund <kirre.bylund@gmail.com>
2025-10-02 11:54:50 +0200

    fix: Stop using deprecated loglevel in samples

--- Samples~/LootLockerExamples/Scripts/LootLockerSettingsOverrider.cs
+++ Samples~/LootLockerExamples/Scripts/LootLockerSettingsOverrider.cs
@@ -6,7 +6,7 @@ public static class LootLockerSettingsOverrider
     public static void OverrideSettings()
     {
         LootLockerSDKManager.Init("dev_3a04ddea32464ca48226eb821a99e3e4", "0.0.0.1", "2112hpxu");
-        LootLocker.LootLockerConfig.current.currentDebugLevel = LootLocker.LootLockerConfig.DebugLevel.All;
+        LootLocker.LootLockerConfig.current.logLevel = LootLockerLogger.LogLevel.Verbose;
     }
 }
 }
