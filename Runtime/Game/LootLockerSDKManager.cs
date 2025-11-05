7169f96edaf6a33a6dd831972e9d609fa5db9f75
Erik Bylund <kirre.bylund@gmail.com>
2025-11-05 14:34:07 +0100

    fix: Split simple inventories into character and player

--- Runtime/Game/LootLockerSDKManager.cs
+++ Runtime/Game/LootLockerSDKManager.cs
@@ -2838,17 +2838,49 @@ namespace LootLocker.Requests
         /// <param name="forPlayerWithUlid">Optional : Execute the request for the specified player. If not supplied, the default player will be used.</param>
         public static void ListPlayerInventoryWithDefaultParameters(Action<LootLockerSimpleInventoryResponse> onComplete, string forPlayerWithUlid = null)
         {
-            ListPlayerInventory(new LootLockerListSimplifiedInventoryRequest(), 0, 100, 1, onComplete, forPlayerWithUlid);
+            ListPlayerInventory(new LootLockerListSimplifiedInventoryRequest(), 100, 1, onComplete, forPlayerWithUlid);
         }
 
         /// <summary>
         /// List player inventory with minimal response data. Due to looking up less data, this endpoint is significantly faster than GetInventory.
         /// </summary>
         /// <param name="request">Request object containing any filters to apply to the inventory listing.</param>
+        /// <param name="perPage">Optional : Number of items per page.</param>
+        /// <param name="page">Optional : Page number to retrieve.</param>
+        public static void ListPlayerInventory(LootLockerListSimplifiedInventoryRequest request, int perPage, int page, Action<LootLockerSimpleInventoryResponse> onComplete, string forPlayerWithUlid = null)
+        {
+            if (!CheckInitialized(false, forPlayerWithUlid))
+            {
+                onComplete?.Invoke(LootLockerResponseFactory.SDKNotInitializedError<LootLockerSimpleInventoryResponse>(forPlayerWithUlid));
+                return;
+            }
+            var endPoint = LootLockerEndPoints.listSimplifiedInventory;
+
+            var queryParams = new LootLocker.Utilities.HTTP.QueryParamaterBuilder();
+            queryParams.Add("per_page", perPage > 0 ? perPage : 100);
+            queryParams.Add("page", page > 0 ? page : 1);
+
+            LootLockerServerRequest.CallAPI(forPlayerWithUlid, endPoint.endPoint + queryParams.Build(), endPoint.httpMethod, LootLockerJson.SerializeObject(request), onComplete: (serverResponse) => { LootLockerResponse.Deserialize(onComplete, serverResponse); });
+        }
+
+        /// <summary>
+        /// List character inventory with default parameters (no filters, first page, default page size).
+        /// </summary>
+        /// <param name="onComplete">onComplete Action for handling the response</param>
+        /// <param name="forPlayerWithUlid">Optional : Execute the request for the specified player. If not supplied, the default player will be used.</param>
+        public static void ListCharacterInventoryWithDefaultParameters(Action<LootLockerSimpleInventoryResponse> onComplete, string forPlayerWithUlid = null)
+        {
+            ListCharacterInventory(new LootLockerListSimplifiedInventoryRequest(), 0, 100, 1, onComplete, forPlayerWithUlid);
+        }
+
+        /// <summary>
+        /// List character inventory with minimal response data. Due to looking up less data, this endpoint is significantly faster than GetInventory.
+        /// </summary>
+        /// <param name="request">Request object containing any filters to apply to the inventory listing.</param>
         /// <param name="characterId">Optional : Filter inventory by character ID.</param>
         /// <param name="perPage">Optional : Number of items per page.</param>
         /// <param name="page">Optional : Page number to retrieve.</param>
-        public static void ListPlayerInventory(LootLockerListSimplifiedInventoryRequest request, int characterId, int perPage, int page, Action<LootLockerSimpleInventoryResponse> onComplete, string forPlayerWithUlid = null)
+        public static void ListCharacterInventory(LootLockerListSimplifiedInventoryRequest request, int characterId, int perPage, int page, Action<LootLockerSimpleInventoryResponse> onComplete, string forPlayerWithUlid = null)
         {
             if (!CheckInitialized(false, forPlayerWithUlid))
             {
