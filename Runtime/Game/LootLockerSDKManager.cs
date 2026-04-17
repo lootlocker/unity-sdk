4373b04cb270867d9abc78de42c3eb41782a03fe
Erik Bylund <kirre.bylund@gmail.com>
2026-04-17 10:58:30 +0200

    fix(error-report): remove static modifier from local function for older C# compatibility

--- Runtime/Game/LootLockerSDKManager.cs
+++ Runtime/Game/LootLockerSDKManager.cs
@@ -8317,7 +8317,7 @@ namespace LootLocker.Requests
 
             report.UserDescription = userDescription;
 
-            static string[] RedactSensitiveHeaders(string[] headers)
+            string[] RedactSensitiveHeaders(string[] headers)
             {
                 if (headers == null) return null;
                 return headers.Select(h =>
